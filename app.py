fd04894b6df7cb6793e8e948a04d8fa039cf5290
Rio Capollari <rcapollari@oakland.edu>
2023-04-08 17:05:04 -0400

    adds radio for teams

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from nba_api.stats.static import teams
-
 import plotly.express as px
 import pandas as pd
 
@@ -8,37 +7,45 @@ app = Flask(__name__)
 
 # Data obtained from https://simplemaps.com/data/us-cities
 df = pd.read_csv('uscities.csv')
+nba_teams = teams.get_teams()
+nba_cities = [team['city'] for team in nba_teams]
+nba_states = [team['state'] for team in nba_teams]
+nba_team_names = [team['full_name'] for team in nba_teams]
+
+df_filtered = df[df['city'].isin(nba_cities) & df['state_name'].isin(nba_states)]
+df_filtered['team_name'] = [nba_team_names[nba_cities.index(city)] if city in nba_cities else '' for city in df_filtered['city']]
+    
+lakers_df = pd.DataFrame({
+    'city': 'Los Angeles',
+    'lat': 34.0522,
+    'lng': -118.2437,
+    'state_id': ['CA'],
+    'state_name': ['California'],
+    'team_name': ['Los Angeles Lakers'],
+    'population': 12121244
+})
+df_combined = pd.concat([df_filtered, lakers_df], ignore_index=True)
 
 @app.route('/')
 def index():
-    nba_teams = teams.get_teams()
-    nba_cities = [team['city'] for team in nba_teams]
-    nba_states = [team['state'] for team in nba_teams]
-    nba_team_names = [team['full_name'] for team in nba_teams]
 
-    df_filtered = df[df['city'].isin(nba_cities) & df['state_name'].isin(nba_states)]
-    df_filtered['team_name'] = [nba_team_names[nba_cities.index(city)] if city in nba_cities else '' for city in df_filtered['city']]
-    
-    lakers_df = pd.DataFrame({
-        'city': 'Los Angeles',
-        'lat': 34.0522,
-        'lng': -118.2437,
-        'state_id': ['CA'],
-        'state_name': ['California'],
-        'team_name': ['Los Angeles Lakers'],
-        'population': 12121244
-    })
-    df_combined = pd.concat([df_filtered, lakers_df], ignore_index=True)
-
-    fig = px.scatter_geo(df_combined, lat='lat', lon='lng', size='population', color='city',
+    fig = px.scatter_geo(df_combined, lat='lat', lon='lng', size='population', color='state_name',
                          projection='albers usa', scope='north america', hover_name='team_name')
 
-    fig.update_layout(height=800, width=1200)
+    fig.update_layout(height=800, width=800)
     fig.update_traces(hovertemplate='%{hovertext}<extra></extra>')
+    fig.update_layout(showlegend=False, margin={"r":0,"t":0,"l":0,"b":0})
 
     plot = fig.to_html(full_html=False)
 
-    return render_template('index.html', plot=plot)
+    return render_template('index.html', plot=plot, nba_team_names=nba_team_names)
+
+
+@app.route('/gps/<team>')
+def gps(team):
+    return render_template('gps.html', team=team)
+
 
 if __name__ == '__main__':
+    print('starting Flask app', app.name)
     app.run(debug=True)
